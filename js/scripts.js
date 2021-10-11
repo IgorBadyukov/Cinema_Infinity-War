dafbb4bb4185c008c5acbbfd2fdadc853d0688ef
IgorBadyukov <82278555+IgorBadyukov@users.noreply.github.com>
2021-10-11 18:55:56 +0300

    Сделал сидения для кинотеатра, добавил адаптив и анимацию, пофиксил баг с option

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,11 @@
 const schemeSvg=document.querySelector('.scheme-svg');
 const totalPriceTag=document.querySelector('.price-total');
+const menuButton=document.querySelector('.m-menu');
+const menu=document.querySelector('.menu');
 let cost=800;
 let totalPrice=800;
+
+
 schemeSvg.addEventListener('click',(event)=>{
     if(!event.target.classList.contains('booked') && !event.target.classList.contains('light')){
         event.target.classList.toggle('active');
@@ -9,4 +13,8 @@ schemeSvg.addEventListener('click',(event)=>{
         totalPrice=totalSeats*cost;
         totalPriceTag.textContent=totalPrice;
     }
+})
+
+menuButton.addEventListener('click',()=>{
+    menu.classList.toggle('is-open');
 })
